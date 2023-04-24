c892a14f5dd3aab070225f8f0de477a0c6a28f13
jp.figueroa <jp.figueroamoran@gmail.com>
2023-04-24 12:30:46 -0600

    Fix:Se realizó reparación de ejercicios

--- js01/script.js
+++ js01/script.js
@@ -15,8 +15,8 @@ console.log("Valor Máximo: ", Number.MIN_SAFE_INTEGER);
 /* BigInt sirven para representar valores númericos enteros, de los que el tipo number
 no puede representar o no es seguro */
 
-console.log("MAX_SAFE_INTEGER + 1 ", Number.MAX_SAFE_INTEGER+1);
-console.log("MAX_SAFE_INTEGER + 1 ", Number.MAX_SAFE_INTEGER+2);
+console.log("MAX_SAFE_INTEGER + 1 ", Number.MAX_SAFE_INTEGER + 1);
+console.log("MAX_SAFE_INTEGER + 1 ", Number.MAX_SAFE_INTEGER + 2);
 let myBigInt = 9007199254740991n;
 console.log("Valor del dato BigINT: ", myBigInt);
 console.log("MAX_SAFE_INTEGER + 1n: ", myBigInt + 1n);
@@ -28,26 +28,45 @@ console.log("Tipo de variable indefinido: ", myVar);
 
 /* Null Intensionalmente se borra el tipo de dato */
 let myVarNull = null; /* solo se declara */
-myVarNull = "Saludos Sr. PP"; /* Se asigna un dato String */ 
-console.log("tipo de dato myVarNull: ", typeof(myVarNull));/* string */
+myVarNull = "Saludos Sr. PP"; /* Se asigna un dato String */
+console.log("tipo de dato myVarNull: ", typeof (myVarNull));/* string */
 myVarNull = null;
-console.log("tipo de dato myVarNull: ", typeof(myVarNull));/* null */
+console.log("tipo de dato myVarNull: ", typeof (myVarNull));/* null */
 
 /* symbol crea propiedades privadas en los objetos */
 
 /* Tipos de datos Object 
 Object: */
-const misDatos = { 
-/*clave :valor  */
-nombre: "Juan Pablo",
-apellido: "Figueroa",
-edad: 25,
-isBelicoso: false,
-musicaPreferidaPorGenero: {
-    rock:"La célula que explota",
-    corridosTumbados:"Ella baila sola"
-}
+const misDatosDePerfil = {
+    /*clave :valor  */
+    nombre: "Juan Pablo",
+    apellido: "Figueroa",
+    edad: 25,
+    isBelicoso: false,
+    musicaPreferidaPorGenero: {
+        rock: "La célula que explota",
+        corridosTumbados: "Ella baila sola"
+    },
+    nombreCompleto: function fullName(){
+        return this.nombre + " " + this.apellido + " , soy el belicón";
+    }
 };
-console.log("Datos completos: ", misDatos);
-console.log("Nombre: ", misDatos.nombre);
-console.log("Música de fin de semana: ", misDatos.musicaPreferidaPorGenero.corridosTumbados);
+const misDatosDeInstagram= {
+    /*clave :valor  */
+    nombre: "Jupafimo",
+    apellido: "kliny",
+    edad: null,
+    isBelicoso: true,
+    musicaPreferidaPorGenero: {
+        rock: "La célula que explota",
+        corridosTumbados: "Ella baila sola"
+    },
+    nombreCompleto: function fullName(){
+        return this.nombre + " " + this.apellido + " , soy el cumbiachero";
+    }
+};
+console.log("Datos completos: ", misDatosDePerfil);
+console.log("Nombre: ", misDatosDePerfil.nombre);
+console.log("Música de fin de semana: ", misDatosDePerfil.musicaPreferidaPorGenero.corridosTumbados);
+console.log("Nombre Completo: ",  misDatosDePerfil.nombreCompleto());
+console.log("Nombre Completo: ", misDatosDeInstagram.nombreCompleto());
