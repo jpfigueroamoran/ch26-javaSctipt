9e765a7976a912c39c656b4061736d050e471711
jp.figueroa <jp.figueroamoran@gmail.com>
2023-04-26 14:33:06 -0600

    Se realizo un pequeño ejercicio para recordar arrays

--- js04/script.js
+++ js04/script.js
@@ -1,1 +1,15 @@
 console.log("JS04 Arreglos y ciclos");
+ /* Declararun array */
+ const nombresCh26 = ["Sofi", "Alejandro","Ixchel", "Said"];
+ //Usando el constructor de arreglos
+ const apellidosCh26 = new Array("Montellano", "Torres", "Vazquez", "Figueroa");
+ const numCajonesEstacionamiento = new Array(4);
+ const numCohortesActivas = new Array(26,27,28,29);
+
+
+ console.log("Apellido de Juan Pablo" + " "+ apellidosCh26[3]);
+
+ //Agregar un nuevo apellido al final del arreglo apellido
+
+ apellidosCh26.push("Moran");
+ console.log(apellidosCh26);
