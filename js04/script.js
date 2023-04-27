6085df0cbd23fcc1b764de5576a53b2357fd447e
jp.figueroamoran <jp.figueroamoran@gmail.com>
2023-04-27 11:12:55 -0600

    Arreglos de dos dimenciones

--- js04/script.js
+++ js04/script.js
@@ -13,3 +13,54 @@ console.log("JS04 Arreglos y ciclos");
 
  apellidosCh26.push("Moran");
  console.log(apellidosCh26);
+
+ /* --------Arreglos de dos dimensiones------------ */
+ const personasEnCohortes = [ 
+    /* ch26 */["Arath","José", "Karen"    ,"Sofi"],
+    /* ch27 */["Manuel", "María", "Francisco", "Pablo", "Pepe"], 
+    /* ch28 */["Doris", "Brendan","Lucas"]
+];
+console.log("Pesonas ch27 "+ personasEnCohortes[1].join(", "));
+//Accediendo a Francisco
+// const temporalCh27 = personasEnCohorte[1];
+const temporalCh27 = ["Manuel", "María", "Francisco", "Pablo", "Pepe"];
+const paco = temporalCh27[2];
+console.log("Extraer en pasos" + paco)
+console.log ("Paco en Ch27 " + personasEnCohortes[1][2]);
+console.log ("Lucas en Ch27 " + personasEnCohortes[2][2]);
+console.log ("Karen en Ch27 " + personasEnCohortes[0][2]);
+
+personasEnCohortes[1][3] = "Juan";
+console.log("Pesonas ch27 "+ personasEnCohortes[1].join(", "));
+
+//myArray3D =  [ [ [ ] ] [ [ ] ] ];
+
+//Iterar un arreglo de dos dimenciones.
+for (let fila = 0; fila < personasEnCohortes.length; fila++) {
+    //const// element = personasEnCohortes[fila];
+    console.log(`Indice ${fila} ${personasEnCohortes[fila] } `);
+    for (let columna = 0; columna < personasEnCohortes[fila].length; columna++) {      
+        console.log(`Nombre:  ${personasEnCohortes[fila][columna]}`);  
+    }
+}
+
+// Usando forEach
+personasEnCohortes.forEach( fila => fila.forEach( columna =>  console.log(columna) ));
+
+// For of
+//Ejecuta sentencias por cada elemento de un objeto iterable (Arrays, Strings y objetos)
+const myName = "Juan"
+for (let char of myName) {
+    console.log(char);
+}
+
+for (let fila of personasEnCohortes){
+    for (let columna of fila){
+        console.log(columna);
+    }
+}
+
+//----------------Consideraciones para el ciclo for------------------
+// No es necesario, indicar el inicio, la comparación y la expresión final
+
+
