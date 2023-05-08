275c80c4768f635ae2ed5cd5df87802211d58869
jp.figueroamoran <jp.figueroamoran@gmail.com>
2023-05-08 12:45:41 -0600

    Uso de Axios

--- js09/assets/scripts/index.js
+++ js09/assets/scripts/index.js
@@ -9,23 +9,39 @@ const getUsersFetch = () => {
             console.log(users.data)
         } )
         .catch( error => console.log(error) );
-}
+};
 const getUsersAxios = () => {
     axios.get(url)
         .then( users => { 
             console.log(users.data.data)  
         })
         .catch( error => console.log(error) );
-     }
+     };
 
 
 /* --------Usar Axios con async-await------------ */
 const getUsersAxiosAwait = async () => {
     try {
       const users = await axios.get(url);
+      const usersId2 = await axios.get(url);
       console.log(users.data.data); 
+      console.log(usersId2); 
+    } catch (error) {
+     console.error(error);
+    }
+  };
+  // ------- Solicutd Post con Axios. ------------------
+  const createUser = async (userData) => {
+    try {
+      const createdUser = await axios.post(url, userData);
+      console.log(createdUser); // o cualquier otra acción que desees realizar con los datos
     } catch (error) {
       console.error(error);
     }
   }
   
+  const userData = {
+    name: "Juan",
+    email: "juan@example.com",
+    password: "123456"
+  };
